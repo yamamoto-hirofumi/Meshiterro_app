41f96d3b10360f5329c74521b2635bbf8d32e0b8
yamamoto-hirofumi <sun.lionkig.88@gmail.com>
2021-07-11 08:36:55 +0000

    [finish]4-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,11 +11,12 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
    @post_image = PostImage.find(params[:id])
+   @post_comment = PostComment.new
   end
 
   def destroy
@@ -23,7 +24,7 @@ class PostImagesController < ApplicationController
     @post_image.destroy
     redirect_to post_images_path
   end
-  
+
    private
 
   def post_image_params
